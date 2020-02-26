bcb7d2698f316997e529ef9c49c473e36ee3c691
Diogo Bastos <diogoalexbastos@hotmail.com>
2020-02-26 12:04:55 +0000

    map done

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -3,7 +3,13 @@ class ArtsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @arts = policy_scope(Art)
+    @arts = policy_scope(Art.geocoded)
+    @markers = @arts.map do |art|
+      {
+        lat: art.latitude,
+        lng: art.longitude
+      }
+    end
   end
 
   def show; end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,12 @@
 import "bootstrap";
 import "../plugins/flatpickr";
 
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import { initMapbox } from '../plugins/init_mapbox';
+
+initMapbox();
+
 const navbar = document.querySelector('.navbar');
 const btnNav = document.querySelectorAll('.btn-nav');
 
